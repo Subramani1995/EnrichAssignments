c07eb41dab14d74677ee3c7d856c8227daeefab3
Subramani1995 <kishoresubramani123@gmail.com>
2016-01-04 18:52:12 +0530

    Create Oddeventriangle.c

---
+++ Oddeventriangle.c
@@ -0,0 +1,28 @@
+# EnrichAssignments
+// To print the Odd - Even number triangle
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int order,i,j,odd=1,even=2,x=1;
+    scanf("%d",&order);
+    for(i=1;i<=order;i=i+2)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",odd);
+            odd=odd+2;
+        }
+        printf("\n");
+        for(j=1;j<=(i+1);j++)
+        {
+            printf("%d ",even);
+            even=even+2;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
