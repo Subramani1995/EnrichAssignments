c0a1ec5e7ae3e584906748a5f657ef35907b90a9
Subramani1995 <kishoresubramani123@gmail.com>
2016-01-23 18:44:08 +0530

    CODE TO COMPRESS THE GIVEN DATA

--- encode.c
+++ encode.c
@@ -1,53 +1,69 @@
 # EnrichAssignments
-To calculate the determinant of the square matrix using GE method
-
+To encode the given information (compress)
 
 #include<stdio.h>
 #include<string.h>
 int main()
 {
-	char s[100],str[100];
-	int i=0,j=1,count=1,k=0,len=0;
+	char s[100],str[100],conversion[100];
+	int i=0,j=1,count=1,k=0,len=0,conv,check,x,y,l;
 	scanf("%s",s);
-	for(i=0;s[i]!='\0';i++)
-	{
-		len++;
-	}
+	len=strlen(s);
 	i=0;
 	while(s[j]!='\0')
 	{
 		while(s[j]==s[i])
 		{
-		//	printf("%c",s[j]);
 			count++;
 			i++;
 			j++;
 		}
 		if(count>2)
 		{
-			str[k]=s[i];
-			printf("%c",str[k]);
-			str[k+1]=count;
-			printf("%c",str[k+1]);
-			k++;
-		}
+		   	str[k]=s[i];
+		   	// THIS PART IS TO CHECK WHETHER THE COUNT GOES GREATER THAN TEN IF YES, CONVERT IT INTO CHAR
+			check=count;
+			y=0;
+			while(check!=0)
+			{
+		                x=check%10;
+                		conversion[y]=x+'0';
+                		y++;
+                		check/=10;
+			}
+			conversion[y]='\0';
+			for(l=y-1;l>=0;l--)
+			{
+			    str[k+1]=conversion[l];
+			    k++;
+			}
+			k=k+1;
+        	}
+
 		else
 		{
-			while(count--)
+		    while(count--)
 			{
-				str[k]=s[i];
-				printf("%c",str[k]);
+			    str[k]=s[i];
 				k++;
-			}
+            		}
 		}
 		j++;
+		if((s[j]==0))
+		{
+	            str[k]=s[j-1];
+        	    str[k+1]='\0';
+            	    break;
+		}
 		i++;
 		count=1;
-		}
-	
-	printf("%s",str);
+	}
+        printf("%s",str);
 	return 0;
 }
+
+
+
 
 	
 	
