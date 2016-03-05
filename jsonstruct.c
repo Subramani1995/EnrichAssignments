73f68f33b74b6b421a2fb87a2c170217e613cce9
Subramani1995 <kishoresubramani123@gmail.com>
2016-03-05 12:47:44 +0530

    Create jsonstruct.c

---
+++ jsonstruct.c
@@ -0,0 +1,19 @@
+// structure format for printing the given JSON
+struct contact_persons
+{
+    char *contact_person_id;
+    char *first_name;
+    char *last_name;
+    char *e_mail;    
+}*c_p;
+struct contact
+{
+    char *contact_id;
+    char *contact_name;
+    char *street;
+    char *area;
+    char *city;
+    struct contact *contact_persons;
+}*c;
+
+   
